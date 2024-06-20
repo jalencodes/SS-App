385dfd0e470452fd772a673ad93cea5b8ecfcc75
Jalen Vaughn <jalencodes@outlook.com>
2024-06-20 07:50:04 -0400

    Login/Signup Forms connect to database

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Form from './components/Form'
 import Home from './pages/Home'
 import Concerts from './pages/Concerts'
 import SignIn from './pages/Sign-In'
+import SignUp from './pages/Sign-Up'
 import ArtistSearch from './pages/ArtistSearch'
 import { useParams } from 'react-router-dom'
 // import ConnectSpotify from './pages/ConnectSpotify'
@@ -25,6 +26,7 @@ function App() {
         <Route path='/' element={<Home/>}></Route>
         <Route path='/concerts' element={<Concerts/>}></Route>
         <Route path='/signin' element={<SignIn/>}></Route>
+        <Route path='/signup' element={<SignUp/>}></Route>
         {/* <Route path='/search/artist/:artistName' element={<ArtistSearch/>}></Route> */}
         {/* <Route path='/connectSpotify' element={<ConnectSpotify/>}></Route> */}
       </Routes>
--- src/pages/Sign-Up/index.jsx
+++ src/pages/Sign-Up/index.jsx
@@ -1,14 +1,16 @@
-import { useState, useRef, useEffect } from "react"
+import { useState } from "react"
 import { Link } from "react-router-dom"
 import axios from "axios"
 
-
-
-export default function SignInForm()
+export default function SignUpForm()
 {
     const [formData, setFormData] = useState({
+        name: '',
+        username: '',
         email: '',
-        password: ''
+        password: '',
+        city: '',
+        state: ''
     })
 
     function handleChange(e) {
@@ -20,25 +22,35 @@ export default function SignInForm()
 
     async function handleSubmit()
     {
-        const requestURL = "http://localhost:8888/api/user/login"
+        const requestURL = "http://localhost:8888/api/user/signup"
         const response = await axios.post(requestURL, formData)
         const user = response.data
         console.log(user);
     }
 
+    
 
 
     return (
         <>
             <div>
+                <label>Name</label>
+                <input type="text" placeholder="Name" onChange={handleChange} name="name" />
                 <label>Email Address</label>
                 <input type="text" placeholder="Enter email" onChange={handleChange} name="email"/>
+                <label>Username</label>
+                <input type="text" placeholder="Username" onChange={handleChange} name="username" />
                 <label>Password</label>
                 <input type="text" placeholder="Password" onChange={handleChange} name="password" />
+                <label>City</label>
+                <input type="text" placeholder="City" onChange={handleChange} name="city" />
+                <label>State</label>
+                <input type="text" placeholder="State" onChange={handleChange} name="state" />
                 <button onClick={handleSubmit}>Submit</button>
+
             </div>
 
-            <Link to="/signup">Not a member? Join the Setlist Seeker Family </Link>
+            <Link to="/signin">Already in the Setlist Seeker Family? Sign In Here! </Link>
         </>
     )
 }
